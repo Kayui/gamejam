15b217eac36ef56bee16c7004d65797a2b0cbce7
Andri Rafn <Andri Rafn>
2017-01-08 16:11:02 +0000

    stuff

--- services/dialogs.js
+++ services/dialogs.js
@@ -154,6 +154,191 @@ class Dialogs {
                     }
                 ]
             },
+            {
+                "id": 5,
+                "situation": "You match with a person on Tinder but find out that the person is a communist.",
+                "precondition": ["right", "begin", "end", "middle"],
+                "options": [
+                    {
+                        "optionID": 0,
+                        "text": "Kill yourself. Everything beautiful is so wrong.",
+                        "lifedrain": 6,
+                        "politicalSwing": -2
+                    },
+                    {
+                        "optionID": 1,
+                        "text": "Go and meet the person.",
+                        "lifedrain": 1,
+                        "politicalSwing": 1,
+                        "postText": "You instantly realize that life without this person is worthless."
+                    },
+                    {
+                        "optionID": 2,
+                        "text": "Send the person a photo of your genitals and Trump's The Art of Deal.",
+                        "lifedrain": 2,
+                        "politicalSwing": -1
+                    }
+                ]
+            },
+            {
+                "id": 6,
+                "situation": "You have spent allt your time lately remixing tracks by Burzum.",
+                "precondition": ["right", "begin", "end", "middle"],
+                "options": [
+                    {
+                        "optionID": 0,
+                        "text": "I love him. He hates the slavish ideology of western civilization as much as I do.",
+                        "lifedrain": 1,
+                        "politicalSwing": -2
+                    },
+                    {
+                        "optionID": 1,
+                        "text": "I happen to like music as hot as he likes wooden churches to be.",
+                        "lifedrain": 1,
+                        "politicalSwing": -1
+                    },
+                    {
+                        "optionID": 2,
+                        "text": "Who is Burzum?",
+                        "lifedrain": 1,
+                        "politicalSwing": 0
+                    },
+                    {
+                        "optionID": 3,
+                        "text": "I listen to angry music so I can be happy and be loved",
+                        "lifedrain": 1,
+                        "politicalSwing": 1
+                    }
+                ]
+            },
+            {
+                "id": 7,
+                "situation": "Your new lover tells you that you are hopelessly dull and doesn't want to see you again.",
+                "precondition": ["right", "begin", "end", "middle"],
+                "options": [
+                    {
+                        "optionID": 0,
+                        "text": "My lover was worth what I payed. Nothing",
+                        "lifedrain": 3,
+                        "politicalSwing": -1
+                    },
+                    {
+                        "optionID": 1,
+                        "text": "Lover?",
+                        "lifedrain": 1,
+                        "politicalSwing": 0
+                    },
+                    {
+                        "optionID": 2,
+                        "text": "Monogamy sucks. I have a lot more.",
+                        "lifedrain": 1,
+                        "politicalSwing": -1
+                    }
+                ]
+            },
+            {
+                "id": 8,
+                "situation": "If life is pointless, then why do we pay for Netflix?",
+                "precondition": ["right", "begin", "end", "middle"],
+                "options": [
+                    {
+                        "optionID": 0,
+                        "text": "Because in heaven those who pay get the best deal.",
+                        "lifedrain": 1,
+                        "politicalSwing": -2
+                    },
+                    {
+                        "optionID": 1,
+                        "text": "Pay for netflix? Streaming-video should be theft!",
+                        "lifedrain": 2,
+                        "politicalSwing": -2
+                    },
+                    {
+                        "optionID": 2,
+                        "text": "Fuck you, I just do and pay what they tell me.",
+                        "lifedrain": 1,
+                        "politicalSwing": 0
+                    }
+                ]
+            },
+            {
+                "id": 9,
+                "situation": "You went to IKEA and...",
+                "precondition": ["left", "begin", "end", "middle"],
+                "options": [
+                    {
+                        "optionID": 0,
+                        "text": "Ate cheap Vegetarian food.",
+                        "lifedrain": 1,
+                        "politicalSwing": 1
+                    },
+                    {
+                        "optionID": 1,
+                        "text": "Lit the christmas goat on fire.",
+                        "lifedrain": 2,
+                        "politicalSwing": 3
+                    },
+                    {
+                        "optionID": 2,
+                        "text": "I am going to sell better and cheaper furniture than they do.",
+                        "lifedrain": 3,
+                        "politicalSwing": -4
+                    }
+                ]
+            },
+            {
+                "id": 10,
+                "situation": "You feel guilty for the pain the social structure has caused.",
+                "precondition": ["left", "begin", "end", "middle"],
+                "options": [
+                    {
+                        "optionID": 0,
+                        "text": "You buy ice cream.",
+                        "lifedrain": 1,
+                        "politicalSwing": -1
+                    },
+                    {
+                        "optionID": 1,
+                        "text": "You write an angry poem and read aloud from your soapbox.",
+                        "lifedrain": 2,
+                        "politicalSwing": 1
+                    },
+                    {
+                        "optionID": 2,
+                        "text": "Go binge drinking in an art gallery.",
+                        "lifedrain": 2,
+                        "politicalSwing": -4,
+                        "postText": "You meet a friend who convince you to form a startup-company."
+                    }
+                ]
+            },
+            {
+                "id": 11,
+                "situation": "The revolution will probably never come",
+                "precondition": ["left", "begin", "end", "middle"],
+                "options": [
+                    {
+                        "optionID": 0,
+                        "text": "So what? I live for the moment.",
+                        "lifedrain": 1,
+                        "politicalSwing": 1
+                    },
+                    {
+                        "optionID": 1,
+                        "text": "Yeah, it will. Just watch.",
+                        "lifedrain": 4,
+                        "politicalSwing": 4,
+                        "postText": "You started the revolution and no one joined."
+                    },
+                    {
+                        "optionID": 2,
+                        "text": "Thank god!",
+                        "lifedrain": 1,
+                        "politicalSwing": -3
+                    }
+                ]
+            }
+
         ]
     }
 
@@ -193,10 +378,10 @@ class Dialogs {
             game = "begin";
         }
 
-        if (pinko.political === 0) {
+        if (pinko.political < 4) {
             political = "right";
         }
-        else if (pinko.political === 9) {
+        else if (pinko.political > 6) {
             political = "left";
         }
 
