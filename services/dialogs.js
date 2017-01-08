6409218f7695cebe7c13025f7df2e2f4f30db45b
Andri Rafn <Andri Rafn>
2017-01-08 15:50:32 +0000

    go

--- services/dialogs.js
+++ services/dialogs.js
@@ -69,7 +69,91 @@ class Dialogs {
                         "politicalSwing": 1
                     }
                 ]
-            }
+            },
+            {
+                "id": 2,
+                "situation": "By average everyone is great.",
+                "precondition": ["center", "begin", "end", "middle"],
+                "options": [
+                    {
+                        "optionID": 0,
+                        "text": "Yes.",
+                        "lifedrain": 1,
+                        "politicalSwing": 0
+                    },
+                    {
+                        "optionID": 1,
+                        "text": "No.",
+                        "lifedrain": 2,
+                        "politicalSwing": 1,
+                    },
+                    {
+                        "optionID": 2,
+                        "text": "Some have more responsibility that others.",
+                        "lifedrain": 1,
+                        "politicalSwing": -1,
+                    },
+                    {
+                        "optionID": 3,
+                        "text": "If everyone is dead then yes.",
+                        "lifedrain": 2,
+                        "politicalSwing": -2
+                    }
+                ]
+            },
+            {
+                "id": 3,
+                "situation": "Where do all the lonely people come from?",
+                "precondition": ["center", "begin", "end", "middle"],
+                "options": [
+                    {
+                        "optionID": 0,
+                        "text": "I don't waste time discussing people.",
+                        "lifedrain": 4,
+                        "politicalSwing": -2,
+                        "postText": "you unstick and falter away with a grin"
+                    },
+                    {
+                        "optionID": 1,
+                        "text": "I am the only lonely person in the world.",
+                        "lifedrain": 6,
+                        "politicalSwing": -1,
+                        "postText": "You suddenly realize that nothing changes, just rearranges and kill yourself while listening to KoRn"
+                    },
+                    {
+                        "optionID": 2,
+                        "text": "There are no lonely people. Just lost opportunities.",
+                        "lifedrain": 1,
+                        "politicalSwing": 1,
+                    }
+                ]
+            },
+            {
+                "id": 4,
+                "situation": "You are more average than other people.",
+                "precondition": ["center", "begin", "end", "middle"],
+                "options": [
+                    {
+                        "optionID": 0,
+                        "text": "That makes no sense. People can't be more average than other people.",
+                        "lifedrain": 6,
+                        "politicalSwing": 0,
+                        "postText": "You waste all time overthinking how something can be more average than other things and stop living your life"
+                    },
+                    {
+                        "optionID": 1,
+                        "text": "Yeah but I make more money than they do.",
+                        "lifedrain": 1,
+                        "politicalSwing": -2,
+                    },
+                    {
+                        "optionID": 2,
+                        "text": "Everyone should strive to be like me.",
+                        "lifedrain": 1,
+                        "politicalSwing": 3
+                    }
+                ]
+            },
         ]
     }
 
@@ -85,7 +169,7 @@ class Dialogs {
                         break;
                     }
                 }
-                if(found){
+                if (found) {
                     console.log(option);
                     return app.services.Database.updatePinko(pinkoID, option.lifedrain, option.politicalSwing);
                 }
@@ -93,7 +177,7 @@ class Dialogs {
         }
 
         // failsafe, no change
-        return {"hey": "go away"};//app.services.Database.getPinkos(pinkoID);
+        return { "hey": "go away" };//app.services.Database.getPinkos(pinkoID);
     }
 
     getDialog(pinkoID) {
