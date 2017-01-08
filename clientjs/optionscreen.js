1ead17d55b6bdbc61097af9bf2f35f7182042646
lommi <jonorn@gmail.com>
2017-01-08 15:03:32 +0000

    Client side adds

--- clientjs/optionscreen.js
+++ clientjs/optionscreen.js
@@ -1,5 +1,11 @@
 var getnewoption = function() {
-  optionscreen();
+  // http://localhost:8080/pinkos/1/dialog
+  $.ajax({
+  url: "/pinkos/"+pinkosData.id+"/dialog",
+  success: function(data) {
+    optionScreen(data);
+  }
+  });
 };
 
 
@@ -7,16 +13,139 @@ let currentOptions = [];
 let currentId = null;
 let currentOption = 0;
 
+var sendOption = function(id, optionID) {
+   keyhandler = function(key) {
+   };
+  // http://localhost:8080/pinkos/1/dialog/2
+  $.ajax({
+  url: "/pinkos/"+pinkosData.id+"/dialog/"+id+"/"+optionID,
+  success: function(data) {
+    initSplash(resetApp);
+  }
+  });
+}
+
 var pickOption = function() {
-  initSplash(function() {});
+  // initSplash(function() {});
+  var option = currentOptions[Object.keys(currentOptions)[currentOption]];
+  console.log(option);
+  option.id = currentId;
+  if (option.postText === undefined) {
+      sendOption(option.id, option.optionID);
+      return
+  }
+
+  showDialog(option);
+
+}
+
+var showDialog = function(option)
+{
+  document.body.innerHTML = '';
+
+  keyhandler = function(key) {
+    sendOption(option.id, option.optionID);
+  };
+  app.render = new Render(document.body, true);
+  app.mainholder = new Render(app.render.element);
+  app.holder = new Render(app.mainholder.element);
+
+  app.mainholder.element.style.height = "auto";
+  app.mainholder.marginTop = "auto";
+  app.mainholder.marginBottom = "auto";
+
+  app.holder.element.style.height = "auto";
+
+  var holder = document.createElement("div");
+  holder.style.height = "100%";
+  holder.style.maxWidth = "480px";
+  holder.style.width = "90%";
+  holder.style.marginTop = "12%";
+  holder.style.marginLeft =  "auto";
+  holder.style.marginRight = "auto";
+  holder.style.marginBottom = "4vh";
+  holder.style.display = "flex";
+  app.holder.element.appendChild(holder);
+
+  var character = new DrawCharacterBox(pinkosData, "", holder);
+
+  app.titleHolder = document.createElement('div');
+  app.titleHolder.style.width = "100%";
+  app.titleHolder.className = "";
+
+  var text = new DrawText(option.postText, {size: 0, center: true, color: 5});
+  app.titleHolder.appendChild(text.object);
+
+  app.mainholder.element.appendChild(app.titleHolder);
+
+  app.nextLine = document.createElement("div");
+  app.nextLine.className = "bottomline";
+  app.nextLine.innerHTML ="Press any key to continue. . . ";
+
+  app.mainholder.element.appendChild(app.nextLine);
+
 }
 
 
+var showDeadDialog = function(option)
+{
+  document.body.innerHTML = '';
 
-var optionscreen = function(id, text, options) {
-   id = id || 1;
-   text = text || "This happend my dear friend you really should do something about it";
-   options = options || [{id: 1, text: "hehehe"}, {id: 2, text: "hehehe"}, {id: 3, text: "hehehe"}, {id: 4, text: "hehehe"}]
+  keyhandler = function(key) {
+    // sendOption(option.id, option.optionID);
+    initSplash(resetApp);
+    return;
+  };
+  app.render = new Render(document.body, true);
+  app.mainholder = new Render(app.render.element);
+  app.holder = new Render(app.mainholder.element);
+
+  app.mainholder.element.style.height = "auto";
+  app.mainholder.marginTop = "auto";
+  app.mainholder.marginBottom = "auto";
+
+  app.holder.element.style.height = "auto";
+
+  var holder = document.createElement("div");
+  holder.style.height = "100%";
+  holder.style.maxWidth = "480px";
+  holder.style.width = "90%";
+  holder.style.marginTop = "12%";
+  holder.style.marginLeft =  "auto";
+  holder.style.marginRight = "auto";
+  holder.style.marginBottom = "4vh";
+  holder.style.display = "flex";
+  app.holder.element.appendChild(holder);
+
+  var character = new DrawCharacterBox(pinkosData, "", holder);
+
+  app.titleHolder = document.createElement('div');
+  app.titleHolder.style.width = "100%";
+  app.titleHolder.className = "";
+
+  var text = new DrawText("You are dead", {size: 0, center: true, color: 5});
+  app.titleHolder.appendChild(text.object);
+
+  app.mainholder.element.appendChild(app.titleHolder);
+
+  app.nextLine = document.createElement("div");
+  app.nextLine.className = "bottomline";
+  app.nextLine.innerHTML ="Press any key to continue. . . ";
+
+  app.mainholder.element.appendChild(app.nextLine);
+
+}
+
+var optionScreen = function(args) {
+    if (pinkosData.lifepoints <= 0) {
+      // DEAD
+      showDeadDialog();
+      return;
+    }
+
+   var id = args.id || 1;
+   var text = args.situation || "This happend my dear friend you really should do something about it";
+   var options = args.options || [{id: 1, text: "hehehe"}, {id: 2, text: "hehehe"}, {id: 3, text: "hehehe"}, {id: 4, text: "hehehe"}]
 
    currentOptions = options;
    currentId = id;
